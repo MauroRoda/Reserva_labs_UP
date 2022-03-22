094a23a5ea406691bbb67cae0f26eef6b33faee5
Mauro Roda <Mauro@Mauro-PC.(mauro)>
2022-03-22 18:29:19 -0300

    atualizações dia 22/03

---
+++ sub-telas/Eng-Mecanica/solicitacao.js
@@ -0,0 +1,16 @@
+
+function Professor(){
+    var SelectProf = document.getElementById("valorPf").value;
+    var SelectProf = "Professor selecionado";
+    
+        console.log(SelectProf);
+    
+}
+
+function Aluno(){
+    var SelectAluno = document.getElementById("valorAl").value;
+    var SelectAluno = "Aluno Selecionado";
+    console.log(SelectAluno)
+
+    
+}
