f7b2dc689c847f06fbff8f5401ebe0753ac79395
Mauro Roda <Mauro@Mauro-PC.(mauro)>
2022-03-23 18:02:46 -0300

    atualização de pagina

--- sub-telas/Eng-Mecanica/solicitacao.js
+++ sub-telas/Eng-Mecanica/solicitacao.js
@@ -1,16 +1,36 @@
 
 function Professor(){
-    var SelectProf = document.getElementById("valorPf").value;
-    var SelectProf = "Professor selecionado";
-    
-        console.log(SelectProf);
-    
+    var Prof = document.getElementById("opcao").value;
+    var Prof = 1;
+
+    if (Prof == 1){
+        var NomeDaLabel = 
+            "<label for=" + "RGM" + "class=" + "field-label" + ">" + "Informe seu Nome" + "</label>"+" "
+            +"<input type=" + "number" + "class=" + "field" + "placeholder=" + "Informe seu RGM ou CPF"
+            +"required></input>" ;
+        var Elemento1 = document.getElementById("CriaDiv");
+        Elemento1.innerHTML = NomeDaLabel;
+    }else{
+        Elemento1.innerHTML = "";
+    }
+
 }
 
 function Aluno(){
-    var SelectAluno = document.getElementById("valorAl").value;
-    var SelectAluno = "Aluno Selecionado";
-    console.log(SelectAluno)
+    var Aluno = document.getElementById("opcao").value;
+    var Aluno = 1;
 
-    
+    if (Aluno == 1){
+        var NomeDaLabel = 
+            "<label for=" + "nome" + "class=" + "field-label" + ">" + "Informe seu Nome" + "</label>" + 
+            " " + "<input type=" + "text" + "class=" + "field" + "placeholder=" + "Informe seu RGM ou CPF" + "></input>"
+            +"<br/>" +
+            "<label for=" + "RGM" + "class=" + "field-label" + ">" + "Informe seu RGM ou CPF" + "</label>" + 
+            " " + "<input type=" + "number" + "class=" + "field" + "></input>";
+        var Elemento1 = document.getElementById("CriaDiv");
+        Elemento1.innerHTML = NomeDaLabel;
+        console.log(NomeDaLabel);
+    }else{
+        Elemento1.innerHTML = "";
+    }
 }
